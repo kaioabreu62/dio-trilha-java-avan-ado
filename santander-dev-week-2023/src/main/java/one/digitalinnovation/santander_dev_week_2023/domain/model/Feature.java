2518882dc4b96a26d96c0e352e14e0377b5400dc
Kaio Abreu <kaio.freires.abreu@gmail.com>
2024-07-13 18:33:57 -0300

    Implementação da camada de domínio

---
+++ santander-dev-week-2023/src/main/java/one/digitalinnovation/santander_dev_week_2023/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.santander_dev_week_2023.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ santander-dev-week-2023/src/main/java/one/digitalinnovation/santander_dev_week_2023/domain/model/News.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.santander_dev_week_2023.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
