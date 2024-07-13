c4eb705c53beb30fa4b37728019f2d5c1e551524
Kaio Abreu <kaio.freires.abreu@gmail.com>
2024-07-13 01:30:11 -0300

    Java RESTful API criada para a Santander Dev Week

---
+++ santander-dev-week-2023/src/test/java/one/digitalinnovation/santander_dev_week_2023/ApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.santander_dev_week_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
